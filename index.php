828f0a7896ef826a6c905f4f97a88d6ca1ad5527
SykieChen <chen17@coder17.com>
2018-03-20 02:29:26 +0800

    rebuilt, works with magic

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
-<?php
-	$uname="";
-	$upass="";
-	if(isset($_POST["uname"])){
-		// from login page
-		$uname = $_POST["uname"];
-		$upass = $_POST["upass"];
-		setcookie("uname", base64_encode($uname), time()+60*60*24*365);
-		setcookie("upass", base64_encode($upass), time()+60*60*24*365);
-		// reload to prevent reposting data confirm dialog while refreshing page
-		header("Location: ./");
-		exit;
-	}
-	else {
-		// direct load
-		if (isset($_COOKIE["uname"])){
-			$uname = base64_decode($_COOKIE["uname"]);
-			$upass = base64_decode($_COOKIE["upass"]);
-		}
-		else {
-			header("Location: ./login/");
-			exit;
-		}
-	}
-?>
-
 <!DOCTYPE html>
 <html lang='zh_cn'>
 <head>
@@ -37,48 +11,121 @@
     		top:0px !important;
 		}
 	</style>
-	<script type="text/javascript">
-		var prevent_bust = 0;
-		window.onbeforeunload = function() { prevent_bust++; }
-		var timer = setInterval(function() {
-			if (prevent_bust > 0) {
-				prevent_bust -= 2;
-				window.top.location = '/204.php';
-			}
-		}, 1);
-	</script>
 </head>
 <body style="margin:0">
 	<div id="spacing_top" style="height:16px;"></div>
-	<iframe id="game_frame" name="game_frame" width="800" height="480" frameborder="0" scrolling="no"></iframe>
-	<form id="login_form" name="login_form" method="post" style="display:none" >
-		<input type="hidden" name="login_id" value="<?php echo $uname ?>" />
-		<input type="password" id = "login_pass" name="password" value="<?php echo $upass ?>" />
-		<input type="hidden" name="mode" value="3" />
-		<input type="submit hidden" name="login_submit" id="login_submit" />
-	</form>
-	<script type="text/javascript">
-		function login()
-		{
-			var btn = document.getElementById("login_submit");
-			var frm = document.getElementById("login_form");
-			var ifm = document.getElementById("game_frame");
-			var pwd = document.getElementById("login_pass");
-			// frm.action = "test.html";
-			frm.action = "http://ooi.moe/";
-			frm.target = "game_frame";
-			frm.submit();
-			btn.disabled = "disabled";
-			ifm.onload = function(){
-				window.clearInterval(timer);
-			}
-			pwd.value="";
-			return false; 
+	<?php
+		function l_post($url, $post){
+			global $cookie;
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_HEADER, 1);	// get header
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   // hold return data
+			if (strlen($cookie)) curl_setopt($ch, CURLOPT_COOKIE, $cookie); //if have cookie, set it
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);   //post data
+			$result=curl_exec($ch);
+			if (preg_match('/(?i)Set-Cookie: (.+?);/',$result,$str)){
+				$cookie = $str[1];
+			} //match the cookie
+			curl_close($ch);
+			return $result;
 		}
-		window.onload=login();
-	</script>
+
+		function l_get($url, $ref_url){
+			global $cookie;
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_HEADER, 1);	// get header
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   // hold return data
+			curl_setopt($ch, CURLOPT_REFERER, $ref_url);
+			if (strlen($cookie)) curl_setopt($ch, CURLOPT_COOKIE, $cookie); //if have cookie, set it
+			$result=curl_exec($ch);
+			curl_close($ch);
+			return $result;
+		}
+
+		$uname="";
+		$upass="";
+		// define url
+		$url = "http://ooi.moe/";
+		$urlpoi = $url . "poi";
+		$cookie='';
+		$bypass = false;
+
+		if(isset($_POST["uname"])){
+			// from login page
+			$uname = $_POST["uname"];
+			$upass = $_POST["upass"];
+			setcookie("uname", base64_encode($uname), time()+60*60*24*365);
+			setcookie("upass", base64_encode($upass), time()+60*60*24*365);
+		
+			// for unknown reason, you have to directly access ooi.moe for once to choose the correct server
+			$get_server_html = <<<HTML
+<iframe id="game_frame" name="game_frame" frameborder="0" scrolling="no" sandbox="allow-forms allow-scripts"></iframe>
+<form id="login_form" name="login_form" method="post" style="display:none">
+	<input type="hidden" name="login_id" value="%s" />
+	<input type="password" id = "login_pass" name="password" value="%s" />
+	<input type="hidden" name="mode" value="3" />
+	<input type="submit hidden" name="login_submit" id="login_submit" />
+</form>
+<script type="text/javascript">
+	function login()
+	{
+		var btn = document.getElementById("login_submit");
+		var frm = document.getElementById("login_form");
+		var ifm = document.getElementById("game_frame");
+		var pwd = document.getElementById("login_pass");
+		frm.action = "%s";
+		frm.target = "game_frame";
+		frm.submit();
+		btn.disabled = "disabled";
+		ifm.onload = function(){
+			window.top.location = "/";
+		}
+		pwd.value="";
+		return false; 
+	}
+	window.onload=login();
+</script>
+
+HTML;
+			$get_server_html = sprintf($get_server_html, $uname, $upass, $url);
+			echo($get_server_html);
+			$bypass = true;
+		}
+		else {
+			// direct load
+			if (isset($_COOKIE["uname"])){
+				$uname = base64_decode($_COOKIE["uname"]);
+				$upass = base64_decode($_COOKIE["upass"]);
+			}
+			else {
+				// no cookie
+				header("Location: ./login/");
+				exit;
+			}
+		}
+		if (!$bypass) {
+			// login
+			$post = array(
+				'login_id'=>$uname,
+				'password'=>$upass,
+				'mode'=>3
+			);
+			l_post($url, $post);
+			
+			// get flash src
+			$html = l_get($urlpoi, $url);
+
+			if (preg_match('/<embed id=\"externalswf\"(.+?)<\/embed>/',$html,$src)) {
+				echo($src[0]);
+			}
+			else {
+				// login error
+				header("Location: ./login/");
+				exit;
+			}
+		}
+	?>
 	<div style="display:none">
-	<script src="https://s19.cnzz.com/z_stat.php?id=1265887046&web_id=1265887046" language="JavaScript"></script>
+		<script src="https://s19.cnzz.com/z_stat.php?id=1265887046&web_id=1265887046" language="JavaScript"></script>
 	</div>
-	
 </body>
